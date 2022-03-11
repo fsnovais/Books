204af5ef9344433d416e906bc1234c7fd0fbd0a4
Felipe Novais <felipekun213@gmail.com>
2022-03-11 11:46:22 -0300

    Capitulo 1

---
+++ Casa do Codigo/Python/salario_real.py
@@ -0,0 +1,6 @@
+salario = int(input('Salario? '))
+imposto = float(input('Imposto? (ex:27.5) '))
+
+if not imposto : imposto = 27.5 
+else: imposto = float(imposto)  
+print('O valor final do seu salario e: {}'.format(salario - (salario * impost0 * 0.01)))
